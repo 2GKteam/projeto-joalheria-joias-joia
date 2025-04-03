b9f313af6f8548c8d47de3515636f35e0e69ed19
Gusleme <gustavolemecastro@gmail.com>
2025-04-03 07:45:34 -0300

    alteração com novos atributos na entidade pedido e endereco

--- src/main/java/br/com/joalheriajoiasjoia/app/entities/Endereco.java
+++ src/main/java/br/com/joalheriajoiasjoia/app/entities/Endereco.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -36,87 +38,13 @@ public class Endereco {
 
 	@Column
 	private String complemento;
+	
+	@ManyToOne
+	@JoinColumn(name = "UsuarioEndereco",nullable = false)
+	private Usuario usuarioId; 
+	
 
 	// Contrutores
-	public Endereco() {
-
-	}
-
-	public Endereco(Long id_endereco, String bairro, String rua, String cidade, String numeroCasa, String estado,
-			String cep, String complemento) {
-		this.id_endereco = id_endereco;
-		this.bairro = bairro;
-		this.rua = rua;
-		this.cidade = cidade;
-		this.numeroCasa = numeroCasa;
-		this.estado = estado;
-		this.cep = cep;
-		this.complemento = complemento;
-	}
-
-	// getters setters
-	public Long getId_endereco() {
-		return id_endereco;
-	}
-
-	public void setId_endereco(Long id_endereco) {
-		this.id_endereco = id_endereco;
-	}
-
-	public String getBairro() {
-		return bairro;
-	}
-
-	public void setBairro(String bairro) {
-		this.bairro = bairro;
-	}
-
-	public String getRua() {
-		return rua;
-	}
-
-	public void setRua(String rua) {
-		this.rua = rua;
-	}
-
-	public String getCidade() {
-		return cidade;
-	}
-
-	public void setCidade(String cidade) {
-		this.cidade = cidade;
-	}
-
-	public String getNumeroCasa() {
-		return numeroCasa;
-	}
-
-	public void setNumeroCasa(String numeroCasa) {
-		this.numeroCasa = numeroCasa;
-	}
-
-	public String getEstado() {
-		return estado;
-	}
-
-	public void setEstado(String estado) {
-		this.estado = estado;
-	}
-
-	public String getCep() {
-		return cep;
-	}
-
-	public void setCep(String cep) {
-		this.cep = cep;
-	}
-
-	public String getComplemento() {
-		return complemento;
-	}
-
-	public void setComplemento(String complemento) {
-		this.complemento = complemento;
-	}
+	
 
 }
--- src/main/java/br/com/joalheriajoiasjoia/app/entities/Pedido.java
+++ src/main/java/br/com/joalheriajoiasjoia/app/entities/Pedido.java
@@ -1,5 +1,31 @@
 package br.com.joalheriajoiasjoia.app.entities;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_pedido")
 public class Pedido {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idPedido;
+	
+	@Column
+	private LocalDateTime dataPedido;
+	
+	@Column
+	private String status;
+	
+	@Column
+	private Double total;
+	
 }
+	
+	
