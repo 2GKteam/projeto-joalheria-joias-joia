b6c4c842e75c07db399ba07d5ac9c99732ba1241
kprimissia <kaiqueealan@gmail.com>
2025-04-24 09:07:19 -0300

    Estou tentando arrumar o data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,23 +1,19 @@
-<<<<<<< HEAD
-INSERT IGNORE INTO tb_tipoUsuario (id_TipoUsuario, nome_tipo_usuario) VALUES 
-(1,'Admin'),
-(2, 'Cliente'), 
-(3, 'Vendedor'), 
+INSERT INTO tb_tipoUsuario (id_tipo_usuario, nome_tipo_usuario) VALUES 
+(1, 'Admin'),
+(2, 'Cliente'),
+(3, 'Vendedor'),
 (4, 'Gerente');
 
-INSERT IGNORE INTO tb_categoria_Produto (id_categoria, nome_categoria, descricao_categoria) VALUES
-(1, 'Anéis', 'Acesório para os dedos'),
-(2, 'Colares', 'Acesório para o pescoço'),
-(3, 'Brincos', 'Acesório para as orelhas'),
-(4, 'Pulseiras', 'Acessorio para os braços'),
-(5, 'Relógios', 'Acesório para não atrasar no SENAI');
+-- Categorias de produtos
+INSERT INTO tb_categoria_Produto (id_categoria, nome_categoria, descricao_categoria) VALUES
+(1, 'Anéis', 'Acessório para os dedos'),
+(2, 'Colares', 'Acessório para o pescoço'),
+(3, 'Brincos', 'Acessório para as orelhas'),
+(4, 'Pulseiras', 'Acessório para os braços'),
+(5, 'Relógios', 'Acessório para não atrasar no SENAI');
 
--- Inserindo Tipos 
-INSERT IGNORE INTO tb_tipo_Produto (id_tipoProduto, nome_tipo_produto, descricao_produto) VALUES
+-- Tipos de produtos
+INSERT INTO tb_tipo_Produto (id_tipoProduto, nome_tipo_produto, descricao_produto) VALUES
 (1, 'Ouro', '18k'),
 (2, 'Prata', '750'),
 (3, 'Bijuteria', 'É biju mas é de rico');
-
-=======
->>>>>>> 88779431a140a53c3aa2937f6ed57f02e5038222
-
