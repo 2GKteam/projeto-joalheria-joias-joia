b47c11fcc9f22a4e8b9c5133c9d2aee554f842d0
kprimissia <kaiqueealan@gmail.com>
2025-05-15 08:45:23 -0300

    arrumei o nome do banco de dados no application.propietes.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,58 +1,34 @@
-<<<<<<< HEAD
-insert ignore into tb_tipo_usuario(id_tipo_usuario,nome_tipo_usuario) values
-=======
-<<<<<<< HEAD
 INSERT IGNORE INTO tb_categoria_produto (id_categoria_produto, nome_categoria_produto, descricao_categoria_produto) VALUES
-(1, 'Anéis', 'Acesório para os dedos'),
-(2, 'Colares', 'Acesório para o pescoço'),
-(3, 'Brincos', 'Acesório para as orelhas'),
-(4, 'Pulseiras', 'Acessorio para os braços'),
-(5, 'Relógios', 'Acesório para não atrasar no SENAI');
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-insert ignore into tb_tipoUsuario(idTipoUsuario,nome_tipo_usuario) values
-=======
+(1, 'Anéis', 'Acessório para os dedos'),
+(2, 'Colares', 'Acessório para o pescoço'),
+(3, 'Brincos', 'Acessório para as orelhas'),
+(4, 'Pulseiras', 'Acessório para os braços'),
+(5, 'Relógios', 'Acessório para não atrasar no SENAI');
 
-INSERT IGNORE INTO tb_tipoUsuario (id_TipoUsuario, nome_tipo_usuario) VALUES 
->>>>>>> 2a74676 (criação do banco de dados no mysql)
->>>>>>> 7d8f39a7eb7cb7585cd94a12d9d8004463beeb7f
-(1,'Admin'),
-(2,'Cliente'),
-(3,'Vendedor'),
-(4,'Gerente');
-<<<<<<< HEAD
+INSERT IGNORE INTO tb_tipo_usuario (id_tipo_usuario, nome_tipo_usuario) VALUES 
+(1, 'Admin'),
+(2, 'Cliente'),
+(3, 'Vendedor'),
+(4, 'Gerente');
+
 -- Categorias de produtos
-INSERT IGNORE INTO tb_categoria_produto (id_categoria, nome_categoria, descricao_categoria)
+INSERT IGNORE INTO tb_categoria_produto (id_categoria_produto, nome_categoria_produto, descricao_categoria_produto)
 VALUES
 (1, 'Anéis', 'Acessório para os dedos'),
 (2, 'Colares', 'Acessório para o pescoço'),
 (3, 'Brincos', 'Acessório para as orelhas'),
 (4, 'Pulseiras', 'Acessório para os braços'),
 (5, 'Relógios', 'Acessório para não atrasar no SENAI');
+
 -- Tipos de produtos
-INSERT ignore INTO tb_tipo_produto (id_tipo_produto, nome_tipo_produto, descricao_produto) VALUES
-=======
-=======
-INSERT INTO tb_tipoUsuario (id_tipo_usuario, nome_tipo_usuario) VALUES 
+INSERT INTO tb_tipo_usuario (id_tipo_usuario, nome_tipo_usuario) VALUES 
 (1, 'Admin'),
 (2, 'Cliente'),
 (3, 'Vendedor'),
 (4, 'Gerente');
->>>>>>> 66a647ded53b8e1dd8021174b453513d6950fc17
 
 -- Inserindo Tipos (se não existirem)
 INSERT IGNORE INTO tb_tipo_produto (id_tipo_produto, nome_tipo_produto, descricao_tipo_produto) VALUES
->>>>>>> 7d8f39a7eb7cb7585cd94a12d9d8004463beeb7f
 (1, 'Ouro', '18k'),
 (2, 'Prata', '750'),
-<<<<<<< HEAD
 (3, 'Bijuteria', 'É biju mas é de rico');
-=======
-(3, 'Bijuteria', 'É biju mas é de rico');
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 2a74676 (criação do banco de dados no mysql)
->>>>>>> 66a647ded53b8e1dd8021174b453513d6950fc17
