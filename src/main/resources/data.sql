dae0d26179bdb992ccdd24d458b6018f001bd8be
Gusleme <gustavolemecastro@gmail.com>
2025-05-15 08:06:18 -0300

    banco de dados arrumado

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,27 +1,18 @@
-<<<<<<< HEAD
-insert ignore into tb_tipoUsuario(idTipoUsuario,nome_tipo_usuario) values
+insert ignore into tb_tipo_usuario(id_tipo_usuario,nome_tipo_usuario) values
 (1,'Admin'),
 (2,'Cliente'),
 (3,'Vendedor'),
 (4,'Gerente');
-=======
-INSERT INTO tb_tipoUsuario (id_tipo_usuario, nome_tipo_usuario) VALUES 
-(1, 'Admin'),
-(2, 'Cliente'),
-(3, 'Vendedor'),
-(4, 'Gerente');
-
 -- Categorias de produtos
-INSERT INTO tb_categoria_Produto (id_categoria, nome_categoria, descricao_categoria) VALUES
+INSERT IGNORE INTO tb_categoria_produto (id_categoria, nome_categoria, descricao_categoria)
+VALUES
 (1, 'Anéis', 'Acessório para os dedos'),
 (2, 'Colares', 'Acessório para o pescoço'),
 (3, 'Brincos', 'Acessório para as orelhas'),
 (4, 'Pulseiras', 'Acessório para os braços'),
 (5, 'Relógios', 'Acessório para não atrasar no SENAI');
->>>>>>> b80b64d5fcecd0af077945128f53a6981aff3bd9
-
 -- Tipos de produtos
-INSERT INTO tb_tipo_Produto (id_tipoProduto, nome_tipo_produto, descricao_produto) VALUES
+INSERT ignore INTO tb_tipo_produto (id_tipo_produto, nome_tipo_produto, descricao_produto) VALUES
 (1, 'Ouro', '18k'),
 (2, 'Prata', '750'),
 (3, 'Bijuteria', 'É biju mas é de rico');
