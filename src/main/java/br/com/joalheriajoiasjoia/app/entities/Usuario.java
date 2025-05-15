0ec5a2cac5a9b12d09bebcdfb23c018ae63a9905
Gusleme <gustavolemecastro@gmail.com>
2025-05-15 10:14:22 -0300

    resolução do relacionamento da entidade endereco e produto

--- src/main/java/br/com/joalheriajoiasjoia/app/entities/Usuario.java
+++ src/main/java/br/com/joalheriajoiasjoia/app/entities/Usuario.java
@@ -1,12 +1,14 @@
 package br.com.joalheriajoiasjoia.app.entities;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -35,6 +37,11 @@ public class Usuario {
 	
 	@Column
 	private String senha;
+	
+	@OneToMany(mappedBy = "usuario")
+	private List<Endereco> enderecos;
+
+
 
 	// Construtores
 	public Usuario() {
