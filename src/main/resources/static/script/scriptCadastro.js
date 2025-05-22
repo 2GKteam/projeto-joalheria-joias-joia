88782d9fcd84994a2aa0cc2e7a843c8b95213622
Gusleme <gustavolemecastro@gmail.com>
2025-05-22 10:34:48 -0300

    inicio do script

--- src/main/resources/static/script/scriptCadastro.js
+++ src/main/resources/static/script/scriptCadastro.js
@@ -1,48 +1,61 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById("cadastroClienteForm");
+	const form = document.getElementById("cadastroClienteForm");
 
-  form.addEventListener("submit", async (event) => {
-    event.preventDefault();
+	form.addEventListener("submit", async (event) => {
+		event.preventDefault();
 
-    const nomeUsuario = document.getElementById("nome").value;
-    const cpf = document.getElementById("cpf").value;
-    const email = document.getElementById("email").value;
-    const telefone = document.getElementById("telefone").value;
-    const dataNascimento = document.getElementById("dt_nascimento").value;
-    const senha = document.getElementById("senha").value;
+		const nomeUsuario = document.getElementById("nome").value;
+		const cpf = document.getElementById("cpf").value;
+		const email = document.getElementById("email").value;
+		const telefone = document.getElementById("telefone").value;
+		const dataNascimento = document.getElementById("dt_nascimento").value;
+		const senha = document.getElementById("senha").value;
 
-    try {
-      const response = await fetch("http://localhost:8080/usuarios", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          nomeUsuario,
-          cpf,
-          email,
-          telefone,
-          dataNascimento,
-          senha,
-          tipoUsuario: {
-            id: 2
-          }
-        }),
-      });
+		try {
+			const response = await fetch("http://localhost:8080/usuarios", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify({
+					nomeUsuario,
+					cpf,
+					email,
+					telefone,
+					dataNascimento,
+					senha,
+					tipoUsuario: {
+						id: 2
+					}
+				}),
+			});
+	  
+	  then(response => {
+				if (response.ok) {
+					return response.json();
+				} else {
+					throw new Error('Erro ao cadastrar usuário');
+				}
+			})
+				.then(data => {
+					alert('Usuário cadastrado com sucesso!\nNomeUsuario: ' + data.nomeUsuario);
+					window.location.href = 'index.html';
+				})
 
-      if (!response.ok) {
-        throw new Error('Erro ao cadastrar pessoa');
-      }
 
-      const data = await response.json();
+			if (!response.ok) {
+				throw new Error('Erro ao cadastrar pessoa');
+			}
 
-      localStorage.setItem('pessoaId', data.idUsuario); // Confirme o nome da propriedade retornada pelo backend
+			const data = await response.json();
 
-      window.location.href = './endereco.html';
+			localStorage.setItem('pessoaId', data.idUsuario); // Confirme o nome da propriedade retornada pelo backend
 
-    } catch (error) {
-      console.error('Erro no cadastro:', error);
-      alert('Falha ao cadastrar pessoa. Tente novamente.');
-    }
-  });
+			window.location.href = './endereco.html';
+
+		} catch (error) {
+			console.error('Erro no cadastro:', error);
+			alert('Falha ao cadastrar pessoa. Tente novamente.');
+		}
+	});
 });
