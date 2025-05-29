ba7f8f55e13dd1deb2cec0bf3a6103269ca906e8
kerenStevuax <keren.anastacio@gmail.com>
2025-05-29 08:14:18 -0300

    Troquei o nome e fiz correção no CamelCase do perfilUsuario.js e scriptLogin.js

--- src/main/resources/static/script/scriptLogin.js
+++ src/main/resources/static/script/scriptLogin.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
 		const senha = document.getElementById('senha').value;
 
 
-		fetch('http://localhost:8080/api/usuarios/login', {
+		fetch('http://localhost:8080/usuarios/login', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json'
