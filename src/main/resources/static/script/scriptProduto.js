1c3392a592243c62300f0db6757d48619f94515c
kerenStevuax <keren.anastacio@gmail.com>
2025-06-10 15:15:35 -0300

    Tentativa de buscar a pag por id de acordo com cada produto

--- src/main/resources/static/script/scriptProduto.js
+++ src/main/resources/static/script/scriptProduto.js
@@ -1,18 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-    
-	const form = document.getElementById("cadastroProdutoForm");
+// Aguarda o carregamento da página
+document.addEventListener("DOMContentLoaded", function() {
+	// Seleciona o formulário pelo ID
+    const form = document.getElementById("cadastroProdutoForm");
 
-	form.addEventListener("submit", async (event) => {
+	// Adiciona um evento de submit no formulário
+	form.addEventListener("submit", function(event) {
 			event.preventDefault();
 
+			// Captura os valores digitados nos campos
         const nome = document.getElementById("nome").value;
         const pre = document.getElementById("pre").value;
         const desc = document.getElementById("desc").value;
         const imgUrl = document.getElementById("imgUrl").value;
+		const categoriaProduto = document.getElementById("categoriaProduto").value;
+		const tipoProduto = document.getElementById("tipoProduto").value;
+		const ornamentoProduto = document.getElementById("ornamentoProduto").value
 
-		
-        try {
-			const response = await fetch("http://localhost:8080/produto", {
+
+		// Envia os dados para o backend usando fetch		
+        fetch("http://localhost:8080/produto", {
                 method: "POST",
                 headers: {
 					"Content-Type": "application/json"
@@ -21,18 +27,20 @@ document.addEventListener("DOMContentLoaded", () => {
 					nome, 
 					pre, 
 					desc,
-					imgUrl
+					imgUrl,
+					categoriaProduto,
+					tipoProduto,
+					ornamentoProduto
 				})
-            });
+            })
+			.then(response => {
+							if (!response.ok) {
+								throw new Error('Erro ao cadastrar produto');
+							}
+							return response.json(); // Pega o corpo da resposta (esperando que venha o objeto com id)
+						})
+							// Redireciona para a página de endereço
+							window.location.href = 'index.html';
+						});
 
-            if(response.ok) {
-				window.location.href ="sucesso.html";
-            } else {
-                alert("Falha ao Cadastrar o Produto :(");
-            }
-        	} catch(error) {
-            console.error("Erro ao cadastrar o Produto:",error);
-        }
-		
-    });
-});
+				})
